042537bee53d3d7381e529ff5f7008d3c01dccb7
Wolf1596Games <wolf1596games@gmail.com>
2020-12-07 16:33:27 -0600

    Added controls to the player

---
+++ 10 Seconds To Live/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float jumpForce = 1f;
+    [SerializeField] float jumpRaycastDistance = 1f;
+    [SerializeField] Transform mainCamera;
+
+    //References
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        var camForward = mainCamera.forward;
+        var camRight = mainCamera.right;
+
+        camForward.y = 0;
+        camForward.Normalize();
+        camRight.y = 0;
+        camRight.Normalize();
+
+        var moveDirection = (camForward * v * moveSpeed) + (camRight * h * moveSpeed);
+
+        transform.LookAt(transform.position);
+        rb.velocity = new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z);
+
+        Ray r = new Ray(transform.position, Vector3.down);
+        Debug.DrawLine(r.origin, r.origin + (Vector3.down * jumpRaycastDistance));
+        RaycastHit hit;
+
+        if(Input.GetButtonDown("Jump") && Physics.Raycast(r, out hit, jumpRaycastDistance))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+    }
+}
